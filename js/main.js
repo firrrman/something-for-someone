d21597b242c30274b89edcf54b583361df000432
firrrman <firmanhakim11704@gmail.com>
2025-08-06 15:01:06 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
-    const titleElement = document.getElementById('title');
+    const titles = "HAPPY BIIRTHDAY TO YOU".split("");
+    const titleElement = document.getElementById("title");
     let index = 0;
 
     function appendTitle() {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 200); // 1000ms delay
       }
     }
 
     appendTitle();
 
     clearTimeout(c);
-  }, 1000);
+  }, 500);
 };
